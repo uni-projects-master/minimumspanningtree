0fc4197dedc182a197dff578ed4c883e413e2b54
francescoPenna0 <55707735+francescoPenna0@users.noreply.github.com>
2022-03-28 20:46:27 +0200

    comments added to kruskal with union find

--- kruskal_union_find.py
+++ kruskal_union_find.py
@@ -26,18 +26,22 @@ class Graph:
 class DisjointedSet:
 	def __init__(self, num_vertex):
 		self.num_vertex = num_vertex
+		# create a list to contain all the verteces in the union find
 		self.vertices = [*range(1, self.num_vertex, 1)]
 		self.parent = {}
+		# at the initial state each node is the father of itself
 		for v in self.vertices:
 			self.parent[v] = v
 
 	def find(self, item):
+		# recursevely search the parent of the parent
 		if self.parent[item] == item:
 			return item
 		else:
 			return self.find(self.parent[item])
 
 	def union(self, set1, set2):
+		# update the data structure 
 		root1 = self.find(set1)
 		root2 = self.find(set2)
 		self.parent[root1] = root2
@@ -48,13 +52,19 @@ class DisjointedSet:
 
 
 def kruskal(g):
+	# create the list that will contain the solution
 	A = []
+	# create the disjointed set to handle the cycle detection
 	U = DisjointedSet(g.num_vertex+1)
-	#U.get_disjointed_set()
+
+	# sort the graph by the weight of the nodes and iterate through it
 	sorted_g = {k: v for k, v in sorted(g.edges.items(), key=lambda item: item[1])}
 	for key in sorted_g:
+		# separate the nodes in the key
 		nodes = key.split()
+		# check if the nodes are already in the same set
 		if U.find(int(nodes[0])) != U.find(int(nodes[1])):
+			# they are not in the same set, add the edge to the solution and union of the sets of v and w
 			A.append(key)
 			U.union(int(nodes[0]), int(nodes[1]))
 	return A
