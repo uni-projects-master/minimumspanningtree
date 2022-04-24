b45ba2cd8e30ff05902c8fdbb9c43c34e3068b61
fra pen <44951845+francescopenna@users.noreply.github.com>
2022-04-24 16:46:48 +0200

    Update prim.py

--- prim.py
+++ prim.py
@@ -14,13 +14,14 @@ class vertex:
 
 class Graph:
     def __init__(self, n_ver, n_edges):
-        self.vertecies = []
+        self.vertices = []
         self.num_edges = n_edges
         self.num_vertex = n_ver
         self.edges = {}
+        # CREATE OBJECT VERTEX AND ADD TO VERTICES
         for v in range(1, n_ver+1):
             new_vertex = vertex(name=v)
-            self.vertecies.append(new_vertex)
+            self.vertices.append(new_vertex)
 
     def add_edges(self, list_edges):
         keys = []
@@ -28,10 +29,12 @@ class Graph:
         for i in list_edges:
             edge = i.split()
             if edge[0] != edge[1]:
+                # SINCE GRAPH IS UNDIRECTED, ADD EDGE IN TWO WAYS
                 keys.append(str(edge[0]) + ' ' + str(edge[1]))
                 weights.append(int(edge[2]))
                 keys.append(str(edge[1]) + ' ' + str(edge[0]))
                 weights.append(int(edge[2]))
+
 
         for k in range(len(keys)):
             self.edges[keys[k]] = weights[k]
@@ -40,7 +43,7 @@ class Graph:
         print(self.edges)
 
     def find_vertex(self, v):
-        for i in self.vertecies:
+        for i in self.vertices:
             if i.Name == v:
                 return i
 
@@ -115,29 +118,33 @@ class minHeap:
 
 
 def prim(G):
-    Q = minHeap()
-    A = []
-    A_weight = 0
-    start = random.choice(G.vertecies)
+    # CHOOSE A STARTING POINT AT RANDOM
+    start = random.choice(G.vertices)
     start.Value = 0
-    for ver in G.vertecies:
+    # INITIALIZE HEAP FOR PRIM WITH NODE VALUES = INFINITY
+    Q = minHeap()
+    for ver in G.vertices:
         Q.insert(ver)
-
+    # WHILE EVERY VERTEX IS NOT INCLUDED IN THE TREE
     while not Q.isEmpty():
+        # EXTRACT NODE WITH MINIMUM VALUE AND FIND ITS ADJACENTS
         u = Q.extractMin()
-        A.append(u)
-        A_weight += u.Value
-        #print(u.Value)
         u_adj = G.find_adj(u)
         for v in u_adj:
             current_edge = v.Name + ' ' + u.Name
+            # IF EDGE (u,v) HAS A LOWER COST UPADTE THE VALUE OF v
             if Q.isInMinHeap(v) and G.edges[current_edge] < v.Value:
                 v.Value = G.edges[current_edge]
                 v.Parent = u
                 Q.updateKey(v)
-
-   # for i in A:
-        #A_weight += i.Value
+    # COMPUTING WEIGHT OF THE TREE
+    A_weight = 0
+    for ver in G.vertices:
+        # IF VERTEX IS NOT THE ROOT
+        if ver.Parent is not None:
+            ver_parent = ver.Parent
+            tree_edge = ver.Name + ' ' + ver_parent.Name
+            A_weight += G.edges[tree_edge]
 
     return A_weight
 
@@ -150,6 +157,5 @@ if __name__ == '__main__':
     edge_list = f.read().splitlines()
     g = Graph(int(line[0]), int(line[1]))
     g.add_edges(edge_list)
-    A, A_w = prim(g)
+    A_w = prim(g)
     print(A_w)
-
